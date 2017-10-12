530ede1667634af564a2229d519859a8faf6d83d
Adrianacmy <adrianamullaney@gmail.com>
2017-10-11 23:44:20 -0700

    flask security works

--- views.py
+++ views.py
@@ -9,25 +9,76 @@ from app import app, db
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
-# Create a user to test with
-# @app.before_first_request
-# def create_user():
-#     db.create_all()
-#     user_datastore.create_user(email='admin2@gmail.com', password='admin2')
-#     db.session.commit()
-# 
-# @app.route('/login')
-# @login_required
-# def login():
-#     return redirect('/admin')
+
+# Create customized model view class
+class dgBaseView(ModelView):
+
+    column_display_pk = True
+    page_size = 20
+    can_view_details = True
+    #can_export = False
+    can_export = True
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
 
 
+class regularRbacView(dgBaseView):
 
+    def is_accessible(self):
+
+        # set accessibility...
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+
+            # roles with ascending permissions...
+        if current_user.has_role('superuser'):
+            self.can_create = True
+            self.can_edit = True
+            self.can_delete = True
+            return True
+
+        if current_user.has_role('volunteer'):
+            self.can_edit = True
+            self.can_delete = False
+            self.can_create = False
+            return True
+        
+        return False
+
+
+class SuperView(dgBaseView):
+
+    can_export = True
+
+    def is_accessible(self):
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+        if current_user.has_role('superuser'):
+            self.can_create = True
+            self.can_edit = True
+            self.can_delete = True
+            #self.can_export = True
+            return True
+        return False
 
 class PianoAdminIndexView(AdminIndexView):
     """
     This does the trick rendering the view only if the user is authenticated
     """
+    column_display_pk = True
+    page_size = 20
+    can_view_details = True
+    
     def is_accessible(self):
         if not current_user.is_active or not current_user.is_authenticated:
             return False
@@ -59,7 +110,7 @@ admin = Admin(
     name='pianos', 
     template_mode='bootstrap3',
     base_template='piano_master.html',
-    index_view=PianoAdminIndexView())
+    )
 
 
 # define a context processor for merging flask-admin's template context into the
@@ -74,6 +125,6 @@ def security_context_processor():
     )
 
 
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Role, db.session))
+admin.add_view(SuperView(User, db.session))
+admin.add_view(SuperView(Role, db.session))
 admin.add_view(ModelView(Piano, db.session))
