7fc7748383eae6032fe22464bc8872349d201614
Adrianacmy <adrianamullaney@gmail.com>
2017-10-11 21:29:16 -0700

    base security works

--- config.py
+++ config.py
@@ -5,8 +5,26 @@ import os
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '\xcb\xa2\x92\xa206]\xd0\xdb\xfe\x9e\xa0\xab\x9d\xc4@\xd0\xf1\xa4\x15\xab\x1a)\xca'
-    # SECRET_KEY = os.urandom(25)
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+    # Flask-Security config
+    SECURITY_URL_PREFIX = "/admin"
+    SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
+    SECURITY_PASSWORD_SALT = "ATGUOHAELKiubahiughaerGOJAEGj"
+
+    # Flask-Security URLs, overridden because they don't put a / at the end
+    SECURITY_LOGIN_URL = "/login/"
+    SECURITY_LOGOUT_URL = "/logout/"
+    SECURITY_REGISTER_URL = "/register/"
+
+    SECURITY_POST_LOGIN_VIEW = "/admin/"
+    SECURITY_POST_LOGOUT_VIEW = "/admin/"
+    SECURITY_POST_REGISTER_VIEW = "/admin/"
+
+    # Flask-Security features
+    SECURITY_REGISTERABLE = True
+    SECURITY_SEND_REGISTER_EMAIL = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConfig(BaseConfig):
--- views.py
+++ views.py
@@ -1,18 +1,79 @@
-from app import app, bcrypt, db
-from flask import Flask, request, redirect, render_template, session, flash, url_for, make_response
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, helpers as admin_helpers
 from flask_admin.contrib.sqla import ModelView
+from flask_security import Security, SQLAlchemyUserDatastore, login_required, current_user
+from flask import redirect, render_template, request, url_for, abort
 from models import *
+from app import app, db
 
-# from models import Post, User
-# from forms import LoginForm, RegisterForm, NewPostForm
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
-from flask_login import login_user, login_required, logout_user, LoginManager, current_user
+# Create a user to test with
+# @app.before_first_request
+# def create_user():
+#     db.create_all()
+#     user_datastore.create_user(email='admin2@gmail.com', password='admin2')
+#     db.session.commit()
+# 
+# @app.route('/login')
+# @login_required
+# def login():
+#     return redirect('/admin')
+
+
+
+
+class PianoAdminIndexView(AdminIndexView):
+    """
+    This does the trick rendering the view only if the user is authenticated
+    """
+    def is_accessible(self):
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+
+        if current_user.has_role('superuser'):
+            return True
+
+        return False
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
+
 @app.route('/')
 def index():
-    return 'hello, piano'
+    return render_template('index.html')
 
 
-admin = Admin(app, name='pianos', template_mode='bootstrap3')
+admin = Admin(
+    app, 
+    name='pianos', 
+    template_mode='bootstrap3',
+    base_template='piano_master.html',
+    index_view=PianoAdminIndexView())
+
+
+# define a context processor for merging flask-admin's template context into the
+# flask-security views.
+@security.context_processor
+def security_context_processor():
+    return dict(
+        admin_base_template=admin.base_template,
+        admin_view=admin.index_view,
+        h=admin_helpers,
+        get_url=url_for
+    )
+
+
 admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Role, db.session))
 admin.add_view(ModelView(Piano, db.session))
